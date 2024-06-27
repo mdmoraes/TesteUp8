97b5ff763d687595044a74bbaa722dfd5826bdc5
mdmoraes.mopsus@gmail.com <mdmoraes>
2024-06-27 10:53:36 -0300

    Atualização de dados e testes de cadastros

--- WebAPP/Queries/populando banco de dados.sql
+++ WebAPP/Queries/populando banco de dados.sql
@@ -3,27 +3,27 @@ GO
 
 INSERT INTO estados
 VALUES
-('São Paulo', 'SP'),
+('SÃ£o Paulo', 'SP'),
 ('Rio de Janeiro', 'RJ'),
-('Paraná', 'PR');
+('ParanÃ¡', 'PR');
 GO
 
 INSERT INTO cidades
 VALUES
-('São Paulo', 1),
+('SÃ£o Paulo', 1),
 ('Guarulhos', 1),
-('Mairiporã', 1),
+('MairiporÃ£', 1),
 ('Rio de Janeiro', 2),
-('Niterói', 2),
-('Macaé', 2),
+('NiterÃ³i', 2),
+('MacaÃ©', 2),
 ('Curitiba', 3),
 ('Londrina', 3),
-('Maringá', 3);
+('MaringÃ¡', 3);
 GO
 
 INSERT INTO clientes
 VALUES
 ('11345678900', 'Teste da Silva', '1990-01-10', 'M', 'Rua 123 de junho', 1),
-('22345678900', 'Testando', '1980-05-20', 'M', 'Rua 456 de março', 4),
+('22345678900', 'Testando', '1980-05-20', 'M', 'Rua 456 de marï¿½o', 4),
 ('33345678900', 'Testada', '1970-09-30', 'F', 'Rua 789 de dezembro', 7);
 GO
